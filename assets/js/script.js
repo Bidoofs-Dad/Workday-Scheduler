ae742bdeff7ae3a1056cd9a366366293e3991cc9
Zane Werle <zanewerle@gmail.com>
2023-04-04 14:24:45 -0500

    added the code that changes the background colors based on what time it is currently, changing the classes to 'past' 'present' and 'future'

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,24 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+
+  $(".time-block").each(function(){
+    var currentHour = dayjs().hour();
+    var hour = parseInt($(this).attr("id").split("-")[1]);
+
+    if (hour < currentHour){
+      $(this).addClass("past");
+    }
+    else if (hour === currentHour){
+      $(this).addClass("present");
+    }
+    else {
+      $(this).addClass("future");
+    }
+
+  })
+  
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
